0f05b25beb1468f1046f576b8e2886559adfaed4
Dennis Vidovic <dvidovic@meraki.com>
2022-09-12 16:28:13 -0700

    init commit

---
+++ installer.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+function confirm() {
+  local question="$1"
+  while true; do
+    msg "$question"
+    read -p "[y]es or [n]o (default: no) : " -r answer
+    case "$answer" in
+      y | Y | yes | YES | Yes)
+        return 0
+        ;;
+      n | N | no | NO | No | *[[:blank:]]* | "")
+        return 1
+        ;;
+      *)
+        msg "Please answer [y]es or [n]o."
+        ;;
+    esac
+  done
+}
+
+function main() {
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    git clone git@github.com:d-3nnis/omz-config.git
+}
+
+main
