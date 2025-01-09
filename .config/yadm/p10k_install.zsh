5bab08b7f80d8568e040ae766b9ff2ff012cf99d
Dennis Vidovic <dennis.vidovic@meraki.net>
2025-01-09 16:16:24 -0500

    Updates

--- .config/yadm/p10k_install.zsh
+++ .config/yadm/p10k_install.zsh
@@ -1,7 +1,9 @@
 #!/usr/bin/env zsh
 
 set -e
-
+echo $ZSH_CUSTOM
+echo "^^^^"
+ZSH_CUSTOM="$HOME/.oh-my-zsh/custom"
 if [ ! -d "${ZSH_CUSTOM}" ]; then
     printf "ZSH_CUSTOM not set, is omz missing?"
     exit 1
