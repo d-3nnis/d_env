0a6e361ce64e12ca72ef0b1363dea63fd936d849
Dennis Vidovic <dennis.vidovic@meraki.net>
2025-01-09 16:11:09 -0500

    Updates

--- .config/yadm/p10k_install.zsh
+++ .config/yadm/p10k_install.zsh
@@ -3,21 +3,21 @@
 set -e
 
 if [ ! -d "${ZSH_CUSTOM}" ]; then
-    printf "ZSH_CUSTOM not set, problem!"
+    printf "ZSH_CUSTOM not set, is omz missing?"
     exit 1
 fi
 
 # Check if Powerlevel10k is installed
 if [ ! -d "${ZSH_CUSTOM}/themes/powerlevel10k" ]; then
-    echo "Powerlevel10k not installed, initiating installation..."
+    printf "Powerlevel10k not installed, initiating installation..."
 
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
     ret=$?
     if [ $ret -ne 0 ]; then
-        echo "Failed to install powerlevel10k"
+        printf "Failed to install powerlevel10k"
         exit $ret
     fi
     # Install Powerlevel10k
 else
-    echo "Powerlevel10k is already installed."
+    printf "Powerlevel10k is already installed."
 fi
